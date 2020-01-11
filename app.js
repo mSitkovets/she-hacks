8378d53c2a2b2fed4260c56e98b0d633db56455f
carmelal <carmela.mn.leung@gmail.com>
2020-01-11 14:04:42 -0500

    Unmessing it up :(

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var http = require('http'),
 fs = require('fs');
 
-fs.readFileSync('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
-});
+http.createServer(function (req, res) {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }).listen(8080);
