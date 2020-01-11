ceb53941abf0ad8be0deef0f1ffa6f9d047503d1
carmelal <carmela.mn.leung@gmail.com>
2020-01-11 12:57:40 -0500

    Display HTML using Node.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const http = require('http');
+var http = require('http'),
+fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
