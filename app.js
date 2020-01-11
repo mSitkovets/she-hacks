871cbf65af2562dfb72343254857b78e39482328
Maria Sitkovets <maria.sitkovets@shopify.com>
2020-01-11 16:55:16 -0500

    Added connection between main page to game page

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var http = require('http'),
-fs = require('fs');
+    fs = require('fs');
 
 http.createServer(function (req, res) {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }).listen(8080);
+
+    if (req.url == '/pages/game.html') {
+        fs.readFile('pages/game.html', function (err, data) {
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.write(data);
+            res.end();
+        });
+    } else {
+        // console.log(req.url)
+        fs.readFile('pages/index.html', function (err, data) {
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.write(data);
+            res.end();
+        });
+    }
+}).listen(8080);
